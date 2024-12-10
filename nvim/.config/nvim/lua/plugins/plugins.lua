3b6ed98abc12e090af46412dc318fc804efa5026
Rui Jiang <me@ruijiang.me>
2024-12-10 15:03:02 -0600

    stylized nvim dashboard

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -4,14 +4,79 @@ return {
   { "mrjones2014/smart-splits.nvim", lazy = false },
   {
     "folke/snacks.nvim",
+    lazy = false,
     opts = {
       dashboard = {
+        enabled = true,
+        preset = {
+          header = [[
+██████   █████                   █████   █████  ███                 
+░██████ ░░███                   ░░███   ░░███  ░░░                  
+░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████  
+░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███ 
+░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███ 
+░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███ 
+█████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████
+░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░]],
+          keys = {
+            { icon = " ", key = "e", desc = "Explore Directory", action = ":Neotree" },
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
         sections = {
           { section = "header" },
-          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 1,
+            section = "keys",
+            indent = 2,
+            gap = 1,
+          },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Recent Files",
+            section = "recent_files",
+            limit = 20,
+            indent = 2,
+            padding = 1,
+          },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           { section = "startup" },
+        },
+        width = 68,
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    lazy = false,
+    opts = {
+      spec = {
+        {
+          "<leader>C",
+          group = "CodeSnap",
+          mode = "x",
+        },
+        {
+          "<leader>gS",
+          icon = {
+            icon = "",
+            color = "blue",
+          },
+          mode = "n",
+          desc = "Toggle Split/Join",
         },
       },
     },
@@ -120,27 +185,6 @@ return {
       colorscheme = "catppuccin",
     },
     lazy = false,
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      spec = {
-        {
-          "<leader>C",
-          group = "CodeSnap",
-          mode = "x",
-        },
-        {
-          "<leader>gS",
-          icon = {
-            icon = "",
-            color = "blue",
-          },
-          mode = "n",
-          desc = "Toggle Split/Join",
-        },
-      },
-    },
   },
   {
     "monkoose/neocodeium",
