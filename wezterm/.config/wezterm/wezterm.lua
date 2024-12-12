d51781f0a64f3f3f93c7e14bc6a79d08118e5120
Rui Jiang <me@ruijiang.me>
2024-12-12 15:24:39 -0600

    updated wezterm style

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ config.keys = keys
 
 config.default_prog = { "/usr/bin/fish", "-l" }
 if wezterm.target_triple == "aarch64-apple-darwin" then
-  config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+	config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 end
 
 config.color_scheme = "Catppuccin Frappe"
@@ -25,69 +25,69 @@ config.tab_max_width = 48
 -- or `wezterm cli set-tab-title`, but falls back to the
 -- title of the active pane in that tab.
 local function tab_title(tab_info)
-  local title = tab_info.tab_title
-  if not title or #title == 0 then
-    title = tab_info.active_pane.title
-  end
-  if not title:find(" ") then
-    title = " " .. title
-  elseif title:find(".+@.+:%s.+") then
-    title = "󰣀 " .. title
-  elseif title:find("^sudo ") then
-    title = "󰦝 " .. title
-  elseif title:find("^nv ") or title:find("^nvim ") then
-    title = title:gsub("^nv ", " "):gsub("^nvim ", " ")
-  elseif title:find("^vim ") then
-    title = title:gsub("^vim ", " ")
-  elseif title:find("^yay ") or title:find("^pacman ") then
-    title = title:gsub("^yay ", " "):gsub("^pacman ", " ")
-  elseif title:find("^Yazi:") then
-    title = title:gsub("^Yazi:", " ")
-  elseif title:find("^deno ") then
-    title = title:gsub("^deno ", "🦖 ")
-  elseif title:find("^node ") then
-    title = title:gsub("^node ", " ")
-  elseif title:find("^bundle ") or title:find("^irb ") then
-    title = title:gsub("^bundle ", " "):gsub("^irb ", " ")
-  end
-  return title
+	local title = tab_info.tab_title
+	if not title or #title == 0 then
+		title = tab_info.active_pane.title
+	end
+	if not title:find(" ") then
+		title = " " .. title
+	elseif title:find(".+@.+:%s.+") then
+		title = "󰣀 " .. title
+	elseif title:find("^sudo ") then
+		title = "󰦝 " .. title
+	elseif title:find("^nv ") or title:find("^nvim ") then
+		title = title:gsub("^nv ", " "):gsub("^nvim ", " ")
+	elseif title:find("^vim ") then
+		title = title:gsub("^vim ", " ")
+	elseif title:find("^yay ") or title:find("^pacman ") then
+		title = title:gsub("^yay ", " "):gsub("^pacman ", " ")
+	elseif title:find("^Yazi:") then
+		title = title:gsub("^Yazi:", " ")
+	elseif title:find("^deno ") then
+		title = title:gsub("^deno ", "🦖 ")
+	elseif title:find("^node ") then
+		title = title:gsub("^node ", " ")
+	elseif title:find("^bundle ") or title:find("^irb ") then
+		title = title:gsub("^bundle ", " "):gsub("^irb ", " ")
+	end
+	return title
 end
 
 local color_scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local edge_background = color_scheme.tab_bar.background
-  local edge_foreground = color_scheme.tab_bar.inactive_tab.fg_color
-  local text_background = color_scheme.tab_bar.inactive_tab.fg_color
-  local text_foreground = color_scheme.tab_bar.inactive_tab.bg_color
-  local left_edge = " "
-  local right_edge = " "
+	local edge_background = color_scheme.tab_bar.background
+	local edge_foreground = color_scheme.tab_bar.inactive_tab.fg_color
+	local text_background = color_scheme.tab_bar.inactive_tab.fg_color
+	local text_foreground = color_scheme.tab_bar.inactive_tab.bg_color
+	local left_edge = " "
+	local right_edge = " "
 
-  if tab.is_active then
-    left_edge = " "
-    right_edge = " "
-    edge_foreground = color_scheme.tab_bar.active_tab.bg_color
-    text_background = color_scheme.tab_bar.active_tab.bg_color
-    text_foreground = color_scheme.tab_bar.active_tab.fg_color
-  elseif hover then
-    edge_foreground = color_scheme.tab_bar.inactive_tab_hover.bg_color
-    text_background = color_scheme.tab_bar.inactive_tab_hover.bg_color
-    text_foreground = color_scheme.tab_bar.inactive_tab_hover.fg_color
-  end
+	if tab.is_active then
+		left_edge = " "
+		right_edge = " "
+		edge_foreground = color_scheme.tab_bar.active_tab.bg_color
+		text_background = color_scheme.tab_bar.active_tab.bg_color
+		text_foreground = color_scheme.tab_bar.active_tab.fg_color
+	elseif hover then
+		edge_foreground = color_scheme.tab_bar.inactive_tab_hover.bg_color
+		text_background = color_scheme.tab_bar.inactive_tab_hover.bg_color
+		text_foreground = color_scheme.tab_bar.inactive_tab_hover.fg_color
+	end
 
-  local title = tab_title(tab)
-  title = tab.tab_index + 1 .. " " .. wezterm.truncate_right(title, max_width - 6)
+	local title = tab_title(tab)
+	title = tab.tab_index + 1 .. " " .. wezterm.truncate_right(title, max_width - 6)
 
-  return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = left_edge },
-    { Background = { Color = text_background } },
-    { Foreground = { Color = text_foreground } },
-    { Text = title },
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = right_edge },
-  }
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = left_edge },
+		{ Background = { Color = text_background } },
+		{ Foreground = { Color = text_foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = right_edge },
+	}
 end)
 
 config.enable_wayland = false
