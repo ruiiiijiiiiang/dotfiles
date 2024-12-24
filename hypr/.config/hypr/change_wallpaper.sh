8ce42eddfc14d25a4c4bdcebea4956a9a0bb5317
Rui Jiang <me@ruijiang.me>
2024-12-23 20:36:48 -0600

    added script to auto rotate wallpapers

---
+++ hypr/.config/hypr/change_wallpaper.sh
@@ -0,0 +1,17 @@
+while true; do
+  directory=~/Pictures/Wallpapers
+  monitor_1=$(hyprctl monitors | grep Monitor | head -n 1 | awk '{print $2}')
+  monitor_2=$(hyprctl monitors | grep Monitor | head -n 2 | awk '{print $2}')
+
+  hyprctl hyprpaper unload all
+
+  random_background=$(ls $directory/*.{jpg,png} | shuf -n 1)
+  hyprctl hyprpaper preload $random_background
+  hyprctl hyprpaper wallpaper "$monitor_1, $random_background"
+
+  random_background=$(ls $directory/*.{jpg,png} | shuf -n 1)
+  hyprctl hyprpaper preload $random_background
+  hyprctl hyprpaper wallpaper "$monitor_2, $random_background"
+
+  sleep $((RANDOM % 900 + 300))
+done
---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,10 @@
+require("full-border"):setup()
+require("starship"):setup()
+require("yaziline"):setup({
+	separator_open = "░▒▓",
+	separator_close = "▓▒░",
+	separator_open_thin = "",
+	separator_close_thin = "",
+	separator_head = "",
+	separator_tail = "",
+})
