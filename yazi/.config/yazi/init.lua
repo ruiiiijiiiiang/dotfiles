d785a74976eaad848fce898af5dd26c5f8aed261
Rui Jiang <me@ruijiang.me>
2025-01-20 14:32:25 -0600

    updated hyprland animation

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,12 +1,13 @@
+require("git"):setup()
+require("starship"):setup()
 require("full-border"):setup({
   type = ui.Border.ROUNDED,
 })
-require("starship"):setup()
 require("yaziline"):setup({
-	separator_open = "░▒▓",
-	separator_close = "▓▒░",
-	separator_open_thin = "",
-	separator_close_thin = "",
-	separator_head = "",
-	separator_tail = "",
+  separator_open = "░▒▓",
+  separator_close = "▓▒░",
+  separator_open_thin = "",
+  separator_close_thin = "",
+  separator_head = "",
+  separator_tail = "",
 })
