31019954650b7784eab11a32f4c6c7e8ff858cfc
Rui Jiang <me@ruijiang.me>
2025-02-03 11:33:03 -0600

    added git config; removed lazy-lock

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
     enabled = true, -- check for plugin updates periodically
     notify = true, -- notify on update
   }, -- automatically check for plugin updates
+  lockfile = false,
   performance = {
     rtp = {
       -- disable some rtp plugins
