69392b81e4f792d7e60a36ac56eb6277b77b1f73
Rui <rui@example.com>
2026-05-06 13:54:20 -0500

    updated niri config

--- fish/.config/fish/functions/nix_deploy.fish
+++ fish/.config/fish/functions/nix_deploy.fish
@@ -14,11 +14,15 @@ function nix_deploy
         set mode switch
     end
 
-    set -l common_flags --target-host "root@$host" \
-        --build-host "root@$host" \
+    set -l builder vm-app
+
+    set -l common_flags \
+        --target-host "root@$host" \
+        --build-host "root@$builder" \
         --flake "/home/rui/nixos-config#$host" \
-        --option extra-substituters "https://cache.ruijiang.me https://cache.nixos.org https://nix-community.cachix.org" \
-        --option extra-trusted-public-keys "cache.ruijiang.me-1:uSB517/xV6UnlCkzOYvmCSRG0sOqPPAGla5tY4iSQf0= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        --option builders-use-substitutes true \
+        --option extra-substituters "https://cache.ruijiang.me https://cache.nixos.org https://nix-community.cachix.org https://wezterm.cachix.org https://noctalia.cachix.org" \
+        --option extra-trusted-public-keys "cache.ruijiang.me-1:uSB517/xV6UnlCkzOYvmCSRG0sOqPPAGla5tY4iSQf0= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0= noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
 
     if set -q _flag_debug
         set common_flags $common_flags --show-trace --verbose --print-build-logs
