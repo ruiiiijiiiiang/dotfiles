38ecf91af9bad47518e98ab25874b48a47eafb2e
Rui <rui@example.com>
2026-06-04 17:38:55 -0500

    fixed nvim snack picker cursor bug

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,6 +7,22 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "snacks_picker_input", "snacks_input", "TelescopePrompt", "fzf" },
+  callback = function(args)
+    local bufnr = args.buf
+    pcall(vim.cmd, "ReactiveStop")
+
+    vim.api.nvim_create_autocmd({ "BufLeave", "BufDelete" }, {
+      buffer = bufnr,
+      once = true,
+      callback = function()
+        pcall(vim.cmd, "ReactiveStart")
+      end,
+    })
+  end,
+})
+
 if profile.is("workstation") then
   require("config.profiles.workstation.autocmds")()
 end
